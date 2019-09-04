71de844309a61328aa0b915a8b9a474e921dba50
Urs van Binsbergen <urs.van.binsbergen@hsr.ch>
2019-09-04 11:28:35 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'casfee2019-p2-g4';
+}
