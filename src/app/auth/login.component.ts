cdd85c5e45f01013dbcca36d398d405025ccfa22
Urs van Binsbergen <urs.van.binsbergen@hsr.ch>
2019-09-11 15:53:28 +0200

    enabled also registration and password reset

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -10,12 +10,14 @@ import { AuthService } from './auth.service';
 })
 export class LoginComponent implements OnInit {
     private next: string;
-    title: string = 'Login';
+
+    mode = "login";
+
     username: string;
     pass: string;
     loginMessage: string;
 
-    constructor(private authService: AuthService, private route: ActivatedRoute, private router: Router, private location: Location) {
+    constructor(private authService: AuthService, private route: ActivatedRoute, private router: Router) {
     }
 
     ngOnInit() {
@@ -23,12 +25,31 @@ export class LoginComponent implements OnInit {
     }
 
     async onSubmit() {
-        const result = await this.authService.login(this.username, this.pass);
+        let result;
+        switch (this.mode) {
+            case 'register':
+                result = await this.authService.register(this.username, this.pass);
+                break;
+            case 'resetPassword':
+                result = await this.authService.sendPasswordMail(this.username);
+                break;
+            default:
+                result = await this.authService.login(this.username, this.pass);
+                break;
+        }
         console.log("component got result", result);
         if (result) {
             this.router.navigateByUrl(this.next);
         } else {
             this.loginMessage = "Login failed";
         }
+    }
+
+    register() {
+        this.mode = "register";
+    }
+
+    resetPassword() {
+        this.mode = "resetPassword";
     }
 }
