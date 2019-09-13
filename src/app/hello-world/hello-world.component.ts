66a8d293f68a9754f9869c36f9a121028a4f5709
Schiess, Peter <peter.schiess@sonova.com>
2019-09-13 18:48:06 +0200

    harmonize material layout on all pages

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './hello-world.component.html'
+    templateUrl: './hello-world.component.html',
+    styleUrls: ['./hello-world.component.scss']
 })
 export class HelloWorldComponent {
 
