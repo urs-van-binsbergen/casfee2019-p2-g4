ebc7693ab7c9eeef62fb898c370749a509fcb7c3
Urs van Binsbergen <urs.van.binsbergen@hsr.ch>
2019-11-06 17:42:23 +0100

    error handling

--- src/app/battle/components/battle.component.ts
+++ src/app/battle/components/battle.component.ts
@@ -7,6 +7,8 @@ import { CloudFunctionsService } from 'src/app/backend/cloud-functions.service';
 import { ShootArgs } from '@cloud-api/arguments';
 import { PlayerInfo, PlayerStatus } from '@cloud-api/core-models';
 import * as battleMethods from '../battle-methods';
+import { NotificationService } from 'src/app/auth/notification.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-battle',
@@ -15,9 +17,11 @@ import * as battleMethods from '../battle-methods';
 })
 export class BattleComponent implements OnInit {
 
+    opponentInfo: PlayerInfo | null = null;
     targetBoard: BattleBoard | null = null;
     ownBoard: BattleBoard | null = null;
-    opponentInfo: PlayerInfo | null = null;
+    shootNow = false;
+    waitingForOpponentShoot = false;
     isVictory = false;
     isWaterloo = false;
 
@@ -25,10 +29,16 @@ export class BattleComponent implements OnInit {
         private router: Router,
         private authState: AuthStateService,
         private cloudData: CloudDataService,
-        private cloudFunctions: CloudFunctionsService
+        private cloudFunctions: CloudFunctionsService,
+        private notification: NotificationService,
+        private translate: TranslateService
     ) { }
 
     ngOnInit(): void {
+        this.subscribeData();
+    }
+
+    subscribeData() {
         this.cloudData.getPlayer$(this.authState.currentUser.uid).subscribe(
             player => {
                 if (player && player.battle) {
@@ -37,16 +47,25 @@ export class BattleComponent implements OnInit {
                     this.ownBoard = battleMethods.createOwnBoard(player);
                     this.isVictory = player.playerStatus === PlayerStatus.Victory;
                     this.isWaterloo = player.playerStatus === PlayerStatus.Waterloo;
+                    this.shootNow = this.targetBoard.canShoot;
+                    this.waitingForOpponentShoot = !this.targetBoard.canShoot && 
+                                                    !this.isVictory && 
+                                                    !this.isWaterloo;
                 } else {
                     this.opponentInfo = null;
                     this.targetBoard = null;
                     this.ownBoard = null;
                     this.isVictory = false;
                     this.isWaterloo = false;
+                    this.shootNow = false;
+                    this.waitingForOpponentShoot = false;
                 }
             },
             error => {
-
+                console.log("error")
+                const errorDetail = this.notification.localizeFirebaseError(error);
+                const msg = this.translate.instant('battle.apiError.loading', { errorDetail });
+                this.notification.toastToConfirm(msg);
             }
         );
     }
@@ -65,6 +84,11 @@ export class BattleComponent implements OnInit {
         this.cloudFunctions.shoot(args).toPromise()
             .then(results => {
                 field.shooting = false;
+            })
+            .catch(error => {
+                const errorDetail = this.notification.localizeFirebaseError(error);
+                const msg = this.translate.instant('battle.apiError.shooting', { errorDetail });
+                this.notification.toastToConfirm(msg);
             })
             ;
     }
