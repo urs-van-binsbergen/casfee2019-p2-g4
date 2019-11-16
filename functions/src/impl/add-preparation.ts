f2019b8bc8ebc30aeda539478a6d98546cad43db
Urs van Binsbergen <urs.van.binsbergen@hsr.ch>
2019-11-16 13:36:56 +0100

    check PlayerStatus

--- functions/src/impl/add-preparation.ts
+++ functions/src/impl/add-preparation.ts
@@ -4,7 +4,7 @@ import { PreparationArgs } from '../public/arguments';
 import { authenticate } from '../shared/auth-utils';
 import { toShip, toSize, convertArray } from '../shared/common-argument-converters';
 import COLL from '../public/firestore-collection-name-const';
-import { getData } from '../shared/db-utils';
+import { getData, getDataOrNull } from '../shared/db-utils';
 import { createBoard, isValidBoard } from '../public/core-methods';
 
 export default async function addPreparation(
@@ -17,17 +17,30 @@ export default async function addPreparation(
     const board = createBoard(args.size, args.ships);
     validateBoard(board);
 
-    const userDoc = await db.collection(COLL.USERS).doc(authInfo.uid).get();
-    const user = getData<User>(userDoc);
+    return db.runTransaction(async tx => {
+        const userRef = db.collection(COLL.USERS).doc(authInfo.uid);
+        const playerRef = db.collection(COLL.PLAYERS).doc(authInfo.uid);
+        const wPlayerRef = db.collection(COLL.WAITING_PLAYERS).doc(authInfo.uid);
 
-    const player = createPlayer(user, board);
-    const waitingPlayer = createWaitingPlayer(user);
+        // Preconditions
+        const allowedStatusses = [PlayerStatus.Preparing, PlayerStatus.Victory, PlayerStatus.Waterloo];
+        const existingPlayer = getDataOrNull<Player>(await playerRef.get());
+        if (existingPlayer && !allowedStatusses.includes(existingPlayer.playerStatus)) {
+            throw new HttpsError('failed-precondition',
+                `PlayerStatus '${existingPlayer.playerStatus}' disallows this operation`);
+        }
 
-    const batch = db.batch();
-    batch.set(db.collection(COLL.PLAYERS).doc(authInfo.uid), player);
-    batch.set(db.collection(COLL.WAITING_PLAYERS).doc(authInfo.uid), waitingPlayer);
+        const userDoc = await userRef.get();
+        const user = getData<User>(userDoc);
 
-    return batch.commit();
+        const player = createPlayer(user, board);
+        const wPlayer = createWaitingPlayer(user);
+
+        // --- Do only WRITE after this point! ------------------------
+
+        tx.set(playerRef, player);
+        tx.set(wPlayerRef, wPlayer);
+    });
 }
 
 /*
