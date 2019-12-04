4e17a1ad5a4c878c33ae2396c3770160b46d47fa
Urs van Binsbergen <urs.van.binsbergen@hsr.ch>
2019-12-04 13:06:11 +0100

    refactoring user state

--- src/app/user/components/user.component.ts
+++ src/app/user/components/user.component.ts
@@ -1,54 +1,102 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthStateService, AuthUser } from 'src/app/auth/auth-state.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthStateService } from 'src/app/auth/auth-state.service';
 import { User, PlayerLevel } from '@cloud-api/core-models';
 import { CloudDataService } from 'src/app/backend/cloud-data.service';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Router } from '@angular/router';
+import * as UserState from 'src/app/user/components/user.state';
 
 @Component({
     selector: 'app-user',
     templateUrl: './user.component.html',
     styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
 
-    authUser: AuthUser;
-    user: User;
-    level: string;
+    state: UserState.State;
+
     userSubscription: Subscription;
+    subscribedUid: string;
 
     constructor(
         private authState: AuthStateService,
         private authService: AuthService,
-        private router: Router,
-        private cloudData: CloudDataService
+        private cloudData: CloudDataService,
+        private router: Router
     ) {
 
     }
 
     ngOnInit() {
+        this.updateState(UserState.getInitialState());
+
         this.authState.isLoggedIn$.subscribe(isLoggedIn => {
-            if (this.userSubscription) {
-                this.userSubscription.unsubscribe();
-            }
             if (!isLoggedIn) {
-                this.authUser = null;
-                this.user = null;
-                this.level = null;
+                // Not authenticated
+                this.updateState(UserState.reduceWithUnauthenticedState());
+                this.unsubscribeUserData();
             } else {
-                this.authUser = this.authState.currentUser;
-                const uid = this.authState.currentUser.uid;
-                this.userSubscription = this.cloudData.getUser$(uid).subscribe(user => {
-                    this.user = user;
-                    this.level = PlayerLevel[user.level];
-                });
+                // Authenticated
+                const authUser = this.authState.currentUser;
+                this.updateState(UserState.reduceWithAuthUser(this.state, authUser));
+                this.subscribeUserData(authUser.uid);
             }
         });
     }
 
+    private subscribeUserData(uid: string) {
+        if (this.userSubscription) {
+            if (this.subscribedUid !== uid) {
+                // already subscribed, but to a different uid
+                console.log('already subscribed, but to a different uid. unsubscribe first.')
+                this.userSubscription.unsubscribe();
+            }
+            else {
+                // already subscribed > ok
+                return;
+            }
+        }
+        console.log('SUBSCRIBE');
+        this.userSubscription = this.cloudData.getUser$(uid).subscribe(user => {
+            if (user) {
+                this.updateState(UserState.reduceWithData(this.state, user));
+            } else {
+                this.updateState(UserState.reduceWithMissingData(this.state));
+            }
+        });
+    }
+
+    private updateState(state: UserState.State) {
+        this.state = state;
+
+        // Warnings are showed with a delay only. Otherwise temporary mismatches
+        // between auth and db state are flashing. An actual mismatch is an 
+        // anomaly so we do not care about the flash then. 
+        if (this.state.delayHandle) {
+            setTimeout(() => {
+                this.state = UserState.reduceWithDelayCancel(this.state, state.delayHandle);
+            }, 2000);
+        }
+    }
+
+    private unsubscribeUserData() {
+        if (this.userSubscription) {
+            console.log('UNSUBSCRIBE');
+            this.userSubscription.unsubscribe();
+            this.userSubscription = null;
+        }
+    }
+
+    ngOnDestroy(): void {
+        console.log('DESTROY');
+        this.unsubscribeUserData();
+    }
+
     async logout() {
-        this.authService.logout();
+        this.unsubscribeUserData();
+        this.authService.logout()
+            .then(() => this.router.navigateByUrl('/'));
     }
 
 }
---
+++ src/app/user/components/user.state.ts
@@ -0,0 +1,101 @@
+import { AuthUser } from 'src/app/auth/auth-state.service';
+import { User, PlayerLevel } from '@cloud-api/core-models';
+
+export interface State {
+
+    isAuthenticated: boolean;
+    uid?: string;
+    displayName?: string;
+    email?: string;
+    emailVerified?: boolean;
+
+    isDataLoaded: boolean;
+    isMissingData: boolean;
+    level?: string;
+    dataDisplayName?: string;
+    dataEmail?: string;
+
+    delayHandle: number;
+}
+
+export function getInitialState(): State {
+    console.log('getInitialState');
+    return {
+        isAuthenticated: false,
+        isDataLoaded: false,
+        isMissingData: false,
+        delayHandle: 0
+    };
+}
+
+export function reduceWithAuthUser(state: State, authUser: AuthUser): State {
+    console.log('reduceWithAuthUser');
+    const isAuthChange = state.uid !== authUser.uid; // means we expect the data will arrive soon after
+    return {
+        isAuthenticated: true,
+        uid: authUser.uid,
+        displayName: authUser.displayName,
+        email: authUser.email,
+        emailVerified: authUser.emailVerified,
+
+        isDataLoaded: isAuthChange ? false : state.isDataLoaded,
+        isMissingData: isAuthChange ? false : state.isMissingData,
+        level: isAuthChange ? '...' : state.level,
+        dataDisplayName: isAuthChange ? authUser.displayName : state.dataDisplayName,
+        dataEmail: isAuthChange ? authUser.email : state.dataEmail,
+
+        delayHandle: isAuthChange ? state.delayHandle + 1 : state.delayHandle
+    }
+}
+
+export function reduceWithUnauthenticedState(): State {
+    console.log('reduceWithUnauthenticedState');
+    return {
+        isAuthenticated: false,
+        isDataLoaded: false,
+        isMissingData: false,
+        delayHandle: 0
+    };
+}
+
+export function reduceWithData(state: State, user: User): State {
+    console.log('reduceWithData');
+    return {
+        ...state,
+        isDataLoaded: true,
+        isMissingData: false,
+        level: PlayerLevel[user.level],
+        dataDisplayName: user.displayName,
+        dataEmail: user.email,
+        delayHandle: state.delayHandle + 1
+    }
+}
+
+export function reduceWithMissingData(state: State): State {
+    console.log('reduceWithMissingData');
+    return {
+        ...state,
+        isDataLoaded: true,
+        isMissingData: true,
+        level: undefined,
+        dataDisplayName: undefined,
+        dataEmail: undefined,
+        delayHandle: state.delayHandle + 1
+    };
+}
+
+export function reduceWithDelayCancel(state: State, handle: number): State {
+    if (!handle) {
+        console.log('reduceWithDelayCancel', 'no delay');
+        return state;
+    }
+    if (handle !== state.delayHandle) {
+        console.log('reduceWithDelayCancel', 'other delay is set', handle, state.delayHandle);
+        return state;
+    }
+    console.log('reduceWithDelayCancel', 'cancel', handle);
+    return {
+        ...state,
+        delayHandle: 0
+    }
+}
