4eb2c95d4dccefc36b19ab4928d82ff7b05b9172
Schiess, Peter <peter.schiess@sonova.com>
2019-12-13 12:14:37 +0100

    remove console from game

--- src/app/game/components/game.component.ts
+++ src/app/game/components/game.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CloudDataService } from 'src/app/backend/cloud-data.service';
 import { AuthStateService } from 'src/app/auth/auth-state.service';
 import * as GameState from '../game.state';
+import { MatSnackBar } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     templateUrl: './game.component.html',
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
 
     state: GameState.State;
 
     constructor(
         private authState: AuthStateService,
-        private cloudData: CloudDataService
+        private cloudData: CloudDataService,
+        private snackBar: MatSnackBar,
+        private translate: TranslateService
     ) {
 
     }
@@ -22,17 +26,29 @@ export class GameComponent implements OnInit {
         this.subscribeData();
     }
 
+    ngOnDestroy(): void {
+        this.hideError();
+    }
 
     subscribeData() {
         this.cloudData.getPlayer$(this.authState.currentUser.uid).subscribe(
             player => {
+                this.hideError();
                 this.state = GameState.reduceWithPlayer(this.state, player);
             },
             error => {
-                // TODO error handling
-                console.error(error);
+                this.showError();
             }
         );
+    }
+
+    private hideError() {
+        this.snackBar.dismiss();
+    }
+
+    showError() {
+        const message = this.translate.instant('game.error');
+        this.snackBar.open(message);
     }
 
 }
