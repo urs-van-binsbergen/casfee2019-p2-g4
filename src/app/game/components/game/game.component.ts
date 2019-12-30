cd574ff08f912ca2ef58e58cd379bdcb2e856b7f
Schiess, Peter <peter.schiess@sonova.com>
2019-12-30 21:56:29 +0100

    game module: refactor state

--- src/app/game/components/game/game.component.ts
+++ src/app/game/components/game/game.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
-import { Store } from '@ngxs/store';
+import { Subject, Observable } from 'rxjs';
+import { Store, Select } from '@ngxs/store';
 import { GameState, GameStateModel } from '../../state/game.state';
 import { tap, takeUntil, finalize } from 'rxjs/operators';
 import { GameStatus, updateStatus } from '../../model/game.model';
@@ -12,31 +12,27 @@ import { BindGame, UnbindGame } from '../../state/game.actions';
 export class GameComponent implements OnInit, OnDestroy {
 
     status: GameStatus = {};
-    private destroy$ = new Subject<void>();
+    private _destroy$ = new Subject<void>();
 
-    constructor(
-        private store: Store,
-    ) {
-    }
+    @Select(GameState.state) _state$: Observable<GameStateModel>;
+
+    constructor(private store: Store) { }
 
     ngOnInit(): void {
         this.store.dispatch(new BindGame());
-        this.store.select(GameState.state)
-            .pipe(
-                takeUntil(this.destroy$),
-                tap((state: GameStateModel) => {
-                    this.status = updateStatus(this.status, state.loading, state.unauthenticated, state.player);
-                }),
-                finalize(() => {
-                    this.store.dispatch(new UnbindGame());
-                })
-            )
-            .subscribe()
-            ;
+        this._state$.pipe(
+            takeUntil(this._destroy$),
+            tap((state: GameStateModel) => {
+                this.status = updateStatus(this.status, state.loading, state.unauthenticated, state.player);
+            }),
+            finalize(() => {
+                this.store.dispatch(new UnbindGame());
+            })
+        ).subscribe();
     }
 
     ngOnDestroy(): void {
-        this.destroy$.next();
+        this._destroy$.next();
     }
 
 }
