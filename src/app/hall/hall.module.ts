3c1c55c774d3a1faa15a83cead1c146a53e456f6
Schiess, Peter <peter.schiess@sonova.com>
2019-12-30 23:17:03 +0100

    hall module: folder structure, refactor ngxs state, page-inset

--- src/app/hall/hall.module.ts
+++ src/app/hall/hall.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { HallComponent } from './components/hall.component';
-import { HallEntriesComponent } from './components/hall-entries.component';
-import { HallEntryComponent } from './components/hall-entry.component';
+import { HallEntriesComponent } from './components/entries/hall-entries.component';
+import { HallEntryComponent } from './components/entry/hall-entry.component';
 import { NgxsModule } from '@ngxs/store';
 import { HallState } from './state/hall.state';
 
--- src/app/hall/state/hall.state.ts
+++ src/app/hall/state/hall.state.ts
@@ -1,27 +1,43 @@
 import { Selector, Action, State, StateContext, Store } from '@ngxs/store';
 import { CloudDataService } from 'src/app/backend/cloud-data.service';
-import { GetHallEntries } from './hall.actions';
-import { HallEntry } from '@cloud-api/core-models';
+import { BindHall, UnbindHall, UpdateHall } from './hall.actions';
+import { HallEntry, PlayerLevel } from '@cloud-api/core-models';
+import { Subject, of } from 'rxjs';
+import { takeUntil, tap, catchError, finalize } from 'rxjs/operators';
+import { NotificationService } from 'src/app/shared/notification.service';
+import * as HallMethods from '../model/hall-methods';
 
 export interface HallStateModel {
     loading: boolean;
-    hallEntries: HallEntry[];
+    admirals: HallEntry[];
+    captains: HallEntry[];
+    seamen: HallEntry[];
+    shipboys: HallEntry[];
 }
 
 @State<HallStateModel>({
     name: 'hall',
     defaults: {
         loading: false,
-        hallEntries: []
+        admirals: [],
+        captains: [],
+        seamen: [],
+        shipboys: []
     }
 })
 
 export class HallState {
 
-    constructor(
-        private cloudData: CloudDataService
-    ) { }
+    private _unbind$: Subject<void>;
+    private _observers: number;
 
+    constructor(
+        private cloudData: CloudDataService,
+        private notification: NotificationService
+    ) {
+        this._unbind$ = null;
+        this._observers = 0;
+    }
 
     @Selector()
     public static loading(state: HallStateModel): boolean {
@@ -29,18 +45,67 @@ export class HallState {
     }
 
     @Selector()
-    public static hallEntries(state: HallStateModel): HallEntry[] {
-        return state.hallEntries;
+    public static admirals(state: HallStateModel): HallEntry[] {
+        return state.admirals;
     }
 
-    @Action(GetHallEntries)
-    getHallEntries({ patchState }: StateContext<HallStateModel>) {
-        patchState({ loading: true });
+    @Selector()
+    public static captains(state: HallStateModel): HallEntry[] {
+        return state.captains;
+    }
 
-        return this.cloudData.getHallEntries()
-            .then(hallEntries => {
-                patchState({ loading: false, hallEntries });
-            });
+    @Selector()
+    public static seamen(state: HallStateModel): HallEntry[] {
+        return state.seamen;
+    }
+
+    @Selector()
+    public static shipboys(state: HallStateModel): HallEntry[] {
+        return state.shipboys;
+    }
+
+    @Action(UpdateHall)
+    updateHall(ctx: StateContext<HallStateModel>, action: UpdateHall) {
+        const admirals = HallMethods.filterByLevel(action.entries, PlayerLevel.Admiral);
+        const captains = HallMethods.filterByLevel(action.entries, PlayerLevel.Captain);
+        const seamen = HallMethods.filterByLevel(action.entries, PlayerLevel.Seaman);
+        const shipboys = HallMethods.filterByLevel(action.entries, PlayerLevel.Shipboy);
+        ctx.setState({ loading: false, admirals, captains, seamen, shipboys});
+    }
+
+    @Action(BindHall)
+    bindMatch(ctx: StateContext<HallStateModel>) {
+        this._observers++;
+        if (!(this._unbind$)) {
+            this._unbind$ = new Subject<void>();
+            ctx.patchState({ loading: true });
+            this.cloudData.getHallEntries$().pipe(
+                takeUntil(this._unbind$),
+                tap((entries: HallEntry[]) => {
+                    ctx.dispatch(new UpdateHall(entries));
+                }),
+                catchError((error) => {
+                    ctx.dispatch(new UpdateHall([]));
+                    this.notification.quickErrorToast('hall.error.entries');
+                    return of();
+                }),
+                finalize(() => {
+                    ctx.dispatch(new UpdateHall([]));
+                    this._unbind$ = null;
+                })
+            ).subscribe();
+        }
+    }
+
+    @Action(UnbindHall)
+    unbindMatch(ctx: StateContext<HallStateModel>) {
+        this._observers--;
+        if (this._observers <= 0) {
+            this._observers = 0;
+            if (this._unbind$) {
+                this._unbind$.next();
+            }
+        }
     }
 
 }
