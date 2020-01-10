e655569af89a4dd5838ef336ccff50ee5f5c25d7
Schiess, Peter <peter.schiess@sonova.com>
2020-01-10 12:24:40 +0100

    improve app.e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,22 +2,29 @@ import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display title', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Battleship P2g4');
-  });
+    it('should display title', async () => {
+        const titleEn = 'Battleship P2g4';
+        const titleDe = 'Panzerkreuzer P2g4';
+        await browser.waitForAngularEnabled(false);
+        await page.navigateTo();
+        await expect(page.getTitleText()).toEqual(titleEn);
+        await page.getGermanButton().click();
+        await expect(page.getTitleText()).toEqual(titleDe);
+        await page.getEnglishButton().click();
+        await expect(page.getTitleText()).toEqual(titleEn);
+    });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+    afterEach(async () => {
+        // Assert that there are no errors emitted from the browser
+        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+        expect(logs).not.toContain(jasmine.objectContaining({
+            level: logging.Level.SEVERE,
+        } as logging.Entry));
+    });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,27 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor, ElementFinder } from 'protractor';
+
+const EC = protractor.ExpectedConditions;
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
-  }
+    navigateTo() {
+        return browser.get(browser.baseUrl) as Promise<any>;
+    }
 
-  getTitleText() {
-    return element(by.css('app-root .title')).getText() as Promise<string>;
-  }
+    getTitleText(): Promise<string> {
+        const ele = element(by.css('app-root .title'));
+        browser.wait(EC.visibilityOf(ele), 10000, 'Title text');
+        return ele.getText() as Promise<string>;
+    }
+
+    getEnglishButton(): ElementFinder {
+        const ele = element(by.css('app-root .segmented-control-label[for="EN"]'));
+        browser.wait(EC.visibilityOf(ele), 10000, 'English button');
+        return ele;
+    }
+
+    getGermanButton(): ElementFinder {
+        const ele = element(by.css('app-root .segmented-control-label[for="DE"]'));
+        browser.wait(EC.visibilityOf(ele), 10000, 'German button');
+        return ele;
+    }
 }
